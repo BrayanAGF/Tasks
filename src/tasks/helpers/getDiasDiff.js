87b7dc61522eecaafc878134890e323420aa8b64
Brayan <mrbrayan.bagf@gmail.com>
2023-08-15 09:27:26 -0600

    correccion

--- src/tasks/helpers/getDiasDiff.js
+++ src/tasks/helpers/getDiasDiff.js
@@ -1,8 +1,8 @@
 
 
 export const getDiasDiff = (f1, f2) => {
-    const fecha1 = moment(f1);
+    /* const fecha1 = moment(f1);
     const fecha2 = moment(f2);
-    const dias = fecha2.diff(fecha1, 'days')+1;
-    return dias;
+    const dias = fecha2.diff(fecha1, 'days')+1; */
+    return 1;
 }

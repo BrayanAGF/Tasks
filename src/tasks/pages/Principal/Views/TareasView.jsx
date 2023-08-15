5633c7af004847b55c8f38759b530832ade093b4
Brayan <mrbrayan.bagf@gmail.com>
2023-08-15 16:58:43 -0600

    prueba de visualizacion de imagenes

--- src/tasks/pages/Principal/Views/TareasView.jsx
+++ src/tasks/pages/Principal/Views/TareasView.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardOverflow, Grid, LinearProgress, Link, Typography } from '@mui/joy'
+import { AspectRatio, Box, Card, CardOverflow, Grid, LinearProgress, Link, Typography } from '@mui/joy'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link as Routerlink } from "react-router-dom"
 import { setTareaActiva } from '../../../../store/Tareas/tareasSlice';
@@ -12,30 +12,39 @@ export const TareasView = () => {
         <Grid sx={{ flexGrow: 1 }} container spacing={2} className='animate__animated animate__fadeIn animate__faster'>
             <Grid xs={12}>
                 <Grid container justifyContent="center" spacing={1}>
-                    {Tareas.map((tarea, index) => (
-                        <Grid key={index}>
-                            <Card sx={{ width: { xs: '300px', md: '380px' }, overflow: 'hidden', mb: 1}} variant='outlined'>
-                                <CardOverflow>
-
-                                    <Grid sx={{ position: 'absolute', top: -8, bottom: 0, left: -8, right: 0, width:'105%' }} >
-                                        <LinearProgress determinate value={tarea.progreso}  variant="plain" color="success" sx={{
-                                            "--LinearProgress-radius": "0px"
-                                        }} />
-                                    </Grid>
-
-                                </CardOverflow>
-                                <Grid>
-                                        <Link component={Routerlink} to='/Tarea' onClick={() => dispatch(setTareaActiva(tarea))}>
-                                            <Typography level="h4">{tarea.Nombre}</Typography>
-                                        </Link>
-                                    <Grid container justifyContent='space-between'>
-                                        <Typography><i className="bi bi-list-ul"></i> {tarea.nActividadesRealizadas} / {tarea.nActividades}</Typography>
-                                        <Typography>{tarea.pNombre}</Typography>
-                                    </Grid>
+                    {
+                        Tareas.length === 0
+                            ? <Grid sx={{display: 'grid', placeItems: 'center'}} height='80vh'>
+                                <Grid textAlign='center'>
+                                    <Typography level='h3' fontWeight='bold'>Parece que no tienes tareas por ahora</Typography>
+                                    <img src='../../../../../public/assets/images/Tareas.png' width='128px' height='128px'/>
                                 </Grid>
-                            </Card>
-                        </Grid>
-                    ))}
+                              </Grid>
+                            : Tareas.map((tarea, index) => (
+                                <Grid key={index}>
+                                    <Card sx={{ width: { xs: '300px', md: '380px' }, overflow: 'hidden', mb: 1 }} variant='outlined'>
+                                        <CardOverflow>
+
+                                            <Grid sx={{ position: 'absolute', top: -8, bottom: 0, left: -8, right: 0, width: '105%' }} >
+                                                <LinearProgress determinate value={tarea.progreso} variant="plain" color="success" sx={{
+                                                    "--LinearProgress-radius": "0px"
+                                                }} />
+                                            </Grid>
+
+                                        </CardOverflow>
+                                        <Grid>
+                                            <Link component={Routerlink} to='/Tarea' onClick={() => dispatch(setTareaActiva(tarea))}>
+                                                <Typography level="h4">{tarea.Nombre}</Typography>
+                                            </Link>
+                                            <Grid container justifyContent='space-between'>
+                                                <Typography><i className="bi bi-list-ul"></i> {tarea.nActividadesRealizadas} / {tarea.nActividades}</Typography>
+                                                <Typography>{tarea.pNombre}</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Card>
+                                </Grid>
+                            ))
+                    }
                 </Grid>
             </Grid>
         </Grid>

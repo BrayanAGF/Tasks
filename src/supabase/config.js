e6831f74ea32b4b692c203516a10d6aa6f5f2b0e
Brayan <mrbrayan.bagf@gmail.com>
2023-08-18 16:56:57 -0600

    Tasks v3

--- src/supabase/config.js
+++ src/supabase/config.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://phwdsycbbiudzavpydle.supabase.co"; /* import.meta.env.VITE_PUBLIC_SUPABASE_URL */
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBod2RzeWNiYml1ZHphdnB5ZGxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIzNzkyODEsImV4cCI6MjAwNzk1NTI4MX0.OcXc6XrQ851D_Frc82vPcf4897qBu_QULJvfc6HugsA"/* import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY */
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
